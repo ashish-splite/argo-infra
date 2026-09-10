05cd92436b0b437304e810564a5e15b2e867bf53
Ashish <ashish@Ashishs-MacBook-Pro.local>
2026-09-10 06:54:36 +0530

    jenkins

---
+++ ci-cd/jenkins/resources/jobs/generate.groovy
@@ -0,0 +1,56 @@
+def config = readYaml(
+    file: 'ci-cd/jenkins/applications.yaml'
+)
+
+config.applications.each { app ->
+
+    def appName = app.name
+
+    pipelineJob("applications/${appName}") {
+
+        description("""
+            CI pipeline for ${appName}.
+
+            Source:
+            ${app.source.repository}
+
+            Image:
+            ${app.image.repository}
+
+            GitOps values:
+            ${app.gitops.valuesFile}
+        """.stripIndent())
+
+        definition {
+            cps {
+
+                script("""
+                    pipeline {
+                        agent any
+
+                        stages {
+
+                            stage('Checkout') {
+                                steps {
+                                    echo 'Building ${appName}'
+                                }
+                            }
+
+                            stage('Test Configuration') {
+                                steps {
+                                    echo 'Source repository: ${app.source.repository}'
+                                    echo 'Source branch: ${app.source.branch}'
+                                    echo 'Image repository: ${app.image.repository}'
+                                    echo 'GitOps repository: ${app.gitops.repository}'
+                                    echo 'Values file: ${app.gitops.valuesFile}'
+                                }
+                            }
+                        }
+                    }
+                """)
+
+                sandbox()
+            }
+        }
+    }
+}
